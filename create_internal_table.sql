4e3ee97101c4c5c857528d5a59185eb270d8de73
Sudarshan-Prakash-Chavan <sudarshannc14@gmail.com>
2024-10-22 21:27:21 +0530

    Create create_internal_table.sql

---
+++ create_internal_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE crimedata (
+    id INTEGER,
+    arrest VARCHAR,
+    beat_num INTEGER,
+    block VARCHAR,
+    case_number VARCHAR,
+    community_code INTEGER,
+    date VARCHAR,
+    date_of_update VARCHAR,
+    district_code INTEGER,
+    domestic VARCHAR,
+    fbi_code VARCHAR,
+    iucr_code VARCHAR,
+    latitude REAL,
+    location VARCHAR,
+    location_desc VARCHAR,
+    longitude REAL,
+    ward_no INTEGER,
+    x_coordinate INTEGER,
+    y_coordinate INTEGER,
+    year INTEGER
+);
